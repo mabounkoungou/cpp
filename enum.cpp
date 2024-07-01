1204e88e751e6cafe860c46622e3466387f802f4
raad <raadbouks@gmail.com>
2024-07-01 19:10:10 +0000

    modify pointers

--- enum.cpp
+++ enum.cpp
@@ -5,8 +5,11 @@ int main(){
 
     string car = "Mercedes";
     string* ptr = &car;
+
+
+    *ptr = "Honda";
     //DEREFERENCING
-    cout << "Memory Address is :"<<" "<< &car ;
+    cout << "Memory Address is :"<<" "<< car ;
     cout << "Memory Address with pointer is :"<<" "<< *ptr;
     
     return 0;
