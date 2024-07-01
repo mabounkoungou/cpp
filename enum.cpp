e9fc3414a66d0ab23b6bbb10bd5fc21e5a0a6335
raad <raadbouks@gmail.com>
2024-07-01 17:52:58 +0000

    Commit

--- enum.cpp
+++ enum.cpp
@@ -5,10 +5,11 @@ enum Level{
     LOW = 10,
     MEDIUM = 20,
     HIGH = 30
-}
+};
 
 int main(){
-    
+    enum Level varaible_x = MEDIUM;
+    cout << "Value of varaible_x: " << varaible_x << endl;
 
     return 0;
 }
