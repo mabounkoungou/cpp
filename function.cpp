6519be8dfc1293f88b4417a0282927976acdc571
raad <raadbouks@gmail.com>
2024-07-02 20:11:01 +0000

    Multiple parameters

--- function.cpp
+++ function.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 using namespace std;
 
-void MyFunction(string car = "G Wagon"){
-    cout << "Car is :" << " "<< car <<endl;
+void MyFunction(string car = "Cheverolet", int year = 2024){
+    cout << "Car is :" << " "<< car << "and year is " << year <<endl;
 }
 
 int main(){
-    MyFunction("Ford");
-    MyFunction("BMW");
+    MyFunction("Ford",2023);
+    MyFunction("BMW",2024);
     MyFunction();
-    MyFunction("Mercedes Benz");
+    MyFunction("Mercedes Benz", 2023);
 }
