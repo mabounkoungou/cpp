d3db2b7ae69728cd25cbb3e70ebffcf7b1a75a42
raad <raadbouks@gmail.com>
2024-07-03 19:00:48 +0000

    Update

--- function.cpp
+++ function.cpp
@@ -1,13 +1,2 @@
 #include <iostream>
 using namespace std;
-
-void MyFunction(string car = "Cheverolet", int year = 2024){
-    cout << "Car is :" << " "<< car << " and year is " << year <<endl;
-}
-
-int main(){
-    MyFunction("Ford",2023);
-    MyFunction("BMW",2024);
-    MyFunction();
-    MyFunction("Mercedes Benz", 2023);
-}
