63cade6cafa6299e26a4c891a615478150d30050
raad <raadbouks@gmail.com>
2024-07-06 19:19:09 +0000

    update

--- function.cpp
+++ function.cpp
@@ -1,24 +1,12 @@
-//Pass By Reference
-#include <iostream>
-using namespace std;
+Void PassArrayAsAFunction(int Numbers[5]){
+    for (i = 0; i < 5, i++){
+        cout << Numbers << endl;
 
-void SwapByReference(int &x, int &y ){
-
-    int Swap = x;
-    x = y;
-    y = Swap;
-
+    }
 }
 
-
-
-int main() {
-    int FNum = 10;
-    int SNum = 20;
-    
-    cout << " Before Swap:" <<"First Number is: " << FNum << "Second Number is: " << SNum << endl;
-   //After Swap 
-   SwapByReference(FNum,SNum);
-       cout << " Before Swap:" <<"First Number is: " << FNum << "Second Number is: " << SNum << endl;
-
+int main() { 
+    int Numbers[5] = {10,20,30,40,50}
+    PassArrayAsAFunction(Numbers);
+    cout << NUmbers <<endl;update
 }
