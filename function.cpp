a38e105c6be56be05ab668c57a7bc9499caad1a4
raad <raadbouks@gmail.com>
2024-07-06 19:22:28 +0000

    Update

--- function.cpp
+++ function.cpp
@@ -1,12 +1,11 @@
-Void PassArrayAsAFunction(int Numbers[5]){
-    for (i = 0; i < 5, i++){
-        cout << Numbers << endl;
+void PassArrayAsAFunction(int Numbers[5]){
+    for (int i = 0; i < 5, i++){
+        cout << Numbers[i] << endl;
 
     }
 }
 
 int main() { 
-    int Numbers[5] = {10,20,30,40,50}
+    int Numbers[5] = {10,20,30,40,50};
     PassArrayAsAFunction(Numbers);
-    cout << NUmbers <<endl;update
 }
